46f56a18a096a9eea4f2dfa29c92b3934092263a
PRATHAMKARMARKAR <karmarkarpratham56@gmail.com>
2025-06-15 01:01:46 +0530

    hello13

--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -6,37 +6,47 @@ import { useSearchParams } from 'react-router-dom';
 import axios from 'axios';
 
 function Profile() {
-    const { navigate } = useGeneral();
+    const {navigate} = useGeneral();
+
     const [searchParams] = useSearchParams();
 
-    const callPostLoginAPI = async () => {
-        try {
-            const token = searchParams.get('token');
-            const role = searchParams.get('role');
+    React.useEffect(() => {
+        const token = searchParams.get('token');
+        const role = searchParams.get('role');
 
-            const res = await axios.post(
+        if (token && role) {
+            axios.post(
                 'https://baggagebugs-1.onrender.com/api/v1/user/setCookies',
                 { token, role },
                 { withCredentials: true }
-            );
-            console.log('User session verified:', res.data);
-            navigate('/dashboard');
-        } catch (err) {
-            console.error('Session check failed:', err);
-            navigate('/login');
+            ).then(() => {
+                window.history.replaceState(null, "", "/landingpage");
+            }).catch((err) => {
+                console.error("Cookie setting failed", err);
+            });
         }
-    };
-
-    // React.useEffect(() => {
-    //     callPostLoginAPI();
-    // }, []);
+    }, []);
 
     return (
         <div className='auth_card'>
-            <button onClick={callPostLoginAPI}>Login</button>
-            Logging in ...
+            <div className='profile_container'>
+                <span className='name'>
+                    <Avatar sx={{backgroundColor: 'orangered', textTransform: 'capitalize'}}>A</Avatar>
+                </span>
+                <span className='full_name'>
+                    name
+                </span>
+                <span className='email'>
+                    email@email.com
+                </span>
+            </div>
+            <div className='action'>
+                <Button endIcon={<Logout />} variant='contained' fullWidth onClick={() => navigate('/login')}>
+                    Logout
+                </Button>
+            </div>
         </div>
-    );
+    )
 }
 
-export default Profile;
+export default Profile
