e287af28669c041fc4e0d53f2ae029a74cb71718
PRATHAMKARMARKAR <karmarkarpratham56@gmail.com>
2025-06-15 21:18:06 +0530

    pushing 4

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -3,20 +3,20 @@ import axios from 'axios';
 import Cookies from 'js-cookie';
 
 function Dashboard() {
-  const [token, setToken] = useState(localStorage.getItem('token'));
-  const [role, setRole] = useState(localStorage.getItem('role'));
+  // const [token, setToken] = useState(localStorage.getItem('token'));
+  // const [role, setRole] = useState(localStorage.getItem('role'));
 
-  useEffect(() => {
-    if (token) localStorage.setItem('token', token);
-    if (role) localStorage.setItem('role', role);
-  }, [token, role]);
+  // useEffect(() => {
+  //   if (token) localStorage.setItem('token', token);
+  //   if (role) localStorage.setItem('role', role);
+  // }, [token, role]);
 
-  const clearAuth = () => {
-    setToken(null);
-    setRole(null);
-    localStorage.removeItem('token');
-    localStorage.removeItem('role');
-  };
+  // const clearAuth = () => {
+  //   setToken(null);
+  //   setRole(null);
+  //   localStorage.removeItem('token');
+  //   localStorage.removeItem('role');
+  // };
   return (
     <div>
       <h1 >Dashboard </h1>
