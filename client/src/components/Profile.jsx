91d3940299ddac84640265941cbbc6535a4bd6f0
PRATHAMKARMARKAR <karmarkarpratham56@gmail.com>
2025-06-15 21:15:50 +0530

    pushing 3

--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -8,46 +8,9 @@ import Cookies from 'js-cookie'; // ← import js-cookie
 
 function Profile() {
     const { navigate } = useGeneral();
-    const [searchParams] = useSearchParams();
 
-    const [token, setToken] = useState(null);
-    const [role, setRole] = useState(null);
 
-    // Extract token and role from URL and store in cookies
-    
 
-    // Get token and role from cookies
-    useEffect(() => {
-        const token = searchParams.get('token');
-        const role = searchParams.get('role');
-         setToken(token);
-        setRole(role);
-    }, []);
-
-    // When token and role are ready, make the API call
-    useEffect(() => {
-        if (token && role) {
-            callPostLoginAPI();
-        }
-    }, [token, role]);
-
-    const callPostLoginAPI = async () => {
-        try {
-            // const res = await axios.post(
-            //     'https://baggagebugs-1.onrender.com/api/v1/user/setCookies',
-            //     { token, role },
-            //     { withCredentials: true }
-            // );
-            // console.log('User session verified:', res.data);
-            console.log('bhai mai nhi dikhunga mr india hue',token, role);
-            localStorage.setItem("token", token);
-            localStorage.setItem("role", role);
-            navigate('/dashboard');
-        } catch (err) {
-            console.error('Session check failed:', err);
-            navigate('/login');
-        }
-    };
 
     return (
         <div className='auth_card'>
