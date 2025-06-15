cc198abe7a7c9cee1854c0b74b6d159f14a2fbf3
PRATHAMKARMARKAR <karmarkarpratham56@gmail.com>
2025-06-15 21:27:54 +0530

    pushing 8

--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -1,30 +1,16 @@
-import { Logout } from '@mui/icons-material';
-import { Avatar, Button } from '@mui/material';
-import React, { useEffect, useState } from 'react';
-import useGeneral from '../hooks/useGeneral.js';
-import { useSearchParams } from 'react-router-dom';
-import axios from 'axios';
-import Cookies from 'js-cookie'; // ← import js-cookie
+import React from "react";// ← import js-cookie
 
 function Profile() {
-    const { navigate } = useGeneral();
+  
   const [token, setToken] = useState(localStorage.getItem('token'));
   const [role, setRole] = useState(localStorage.getItem('role'));
 
   useEffect(() => {
     if (token) localStorage.setItem('token', token);
     if (role) localStorage.setItem('role', role);
+    console.log('Token:', token);
+    console.log('Role:', role);
   }, [token, role]);
-
-  const clearAuth = () => {
-    setToken(null);
-    setRole(null);
-    localStorage.removeItem('token');
-    localStorage.removeItem('role');
-  };
-
-
-
     return (
         <div className='auth_card'>
             <p>Logging in...</p>
